dc99d98609656640fe9ba65fb4033212a8a0e2be
mokunshao <mokunshao@foxmail.com>
2019-10-27 17:30:19 +0800

    添加顶部导航栏

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface TopMenu {
+  title: string;
+  readonly link?: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-store';
+  menu: TopMenu[] = [
+    { title: '热门', link: '' },
+    { title: '男装', link: '' },
+    { title: '女装', link: '' },
+    { title: '图书', link: '' },
+    { title: '运动', link: '' },
+    { title: '手机', link: '' },
+    { title: '电脑', link: '' },
+    { title: '美妆', link: '' },
+    { title: '汽车', link: '' },
+    { title: '医药', link: '' },
+    { title: '内衣', link: '' },
+  ];
 }
