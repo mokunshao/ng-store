e4a110322c469182f6e684e6e60bbbb0161babe4
mokunshao <mokunshao@foxmail.com>
2019-10-27 18:11:43 +0800

    添加 app 组件点击事件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,19 @@ interface TopMenu {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  selectedIndex = -1;
   title = 'ng-store';
   menu: TopMenu[] = [
-    { title: '热门', link: '' },
-    { title: '男装', link: '' },
-    { title: '女装', link: '' },
-    { title: '图书', link: '' },
-    { title: '运动', link: '' },
-    { title: '手机', link: '' },
-    { title: '电脑', link: '' },
-    { title: '美妆', link: '' },
-    { title: '汽车', link: '' },
-    { title: '医药', link: '' },
-    { title: '内衣', link: '' },
+    { title: '热门', link: '#' },
+    { title: '男装', link: '#' },
+    { title: '女装', link: '#' },
+    { title: '图书', link: '#' },
+    { title: '运动', link: '#' },
+    { title: '手机', link: '#' },
+    { title: '电脑', link: '#' },
+    { title: '美妆', link: '#' },
+    { title: '汽车', link: '#' },
+    { title: '医药', link: '#' },
+    { title: '内衣', link: '#' },
   ];
 }
